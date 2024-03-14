badbf7b9358365df74333f2695d0109a8aec7ab6
ThuanVan Nguyen <60531224+Nguyen-Van-Thuan@users.noreply.github.com>
2024-03-14 20:53:22 +0700

    update b8

--- convert-project/assets/js/custom.js
+++ convert-project/assets/js/custom.js
@@ -1,3 +1,27 @@
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+$(document).ready(function () {
+
+  $('.owl-carousel').owlCarousel({
+    loop: true, //Vong lap
+    margin: 30, //Khoang cach giua item
+    nav: true, //Thanh dieu huong (Mui ten)
+    dots: true, //Dieu huong (Dau cham tron)
+    autoplay: true, //tu dong chuyen slider
+    autoplayTimeout: 4000, //Thoi chuyen slider (ms)
+
+    responsive: { //Quy dinh SLSP tren cac man hinh
+      0: { // -> 599px
+        items: 1
+      },
+      600: { //600px -> 999px
+        items: 1
+      },
+      1000: {//1000px -> 1499px
+        items: 1
+      },
+      1500: {//1500px tro len
+        items: 1
+      }
+    }
+  })
+
 });
